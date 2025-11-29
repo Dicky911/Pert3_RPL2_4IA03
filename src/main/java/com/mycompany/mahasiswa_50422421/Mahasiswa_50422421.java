028cc442bd7e09d24185e7bad447d282b5079247
Dicky911 <satriodicky71@gmail.com>
2025-11-29 11:44:11 +0700

    Initial Commit

---
+++ src/main/java/com/mycompany/mahasiswa_50422421/Mahasiswa_50422421.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mahasiswa_50422421;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Mahasiswa_50422421 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
